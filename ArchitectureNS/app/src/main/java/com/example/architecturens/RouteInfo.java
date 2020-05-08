86e5c70e93688b411d2b9387c6d6c8e419c9cf92
elena1237 <elenakevac@gmail.com>
2020-05-08 14:13:27 +0200

    Added PlaceInfo and RouteInfo classes

--- ArchitectureNS/app/src/main/java/com/example/architecturens/RouteInfo.java
+++ ArchitectureNS/app/src/main/java/com/example/architecturens/RouteInfo.java
@@ -6,10 +6,19 @@ public final class RouteInfo {
     private final String id;
     private final String title;
     private final List<PlaceInfo> places;
+    private final int duration;
+    private  final String description;
+    private final double kilometres;
+    private final String pictureFileName;
 
-    public RouteInfo(String id, String title, List<PlaceInfo> places) {
+
+    public RouteInfo(String id, String title, List<PlaceInfo> places, int duration, String description, double kilometres, String pictureFileName) {
         this.id = id;
         this.title = title;
         this.places = places;
+        this.duration = duration;
+        this.description = description;
+        this.kilometres = kilometres;
+        this.pictureFileName = pictureFileName;
     }
 }
