7533d3487c10f93b493f4207a885c8c4adec13ff
ivanamar996 <markovic.ivana996@gmail.com>
2020-05-11 12:13:37 +0200

    Add custom marker to map and polyline

--- ArchitectureNS/app/src/main/java/com/example/architecturens/MapsActivity.java
+++ ArchitectureNS/app/src/main/java/com/example/architecturens/MapsActivity.java
@@ -1,31 +1,63 @@
 package com.example.architecturens;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.util.MapUtils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private List<LatLng> placePositions = new ArrayList<LatLng>();
 
     private GoogleMap mMap;
     public static final String PLACE_INFO = "com.example.architecturens.PLACE_INFO";
     private RouteInfo mRouteInfo;
+
+    private Marker customMarker;
+    private LatLng markerLatLng;
+    private Polyline line;
 
 
     @Override
@@ -37,13 +69,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        getIntentAndDisplayValues();
-    }
-
-    private void getIntentAndDisplayValues() {
         Intent intent = getIntent();
         mRouteInfo = intent.getParcelableExtra(PLACE_INFO);
 
+        placePositions.add(new LatLng(45.255790, 19.845772));
+        placePositions.add(new LatLng(45.256132, 19.845423));
+        placePositions.add(new LatLng(45.256283, 19.847117));
     }
 
     /**
@@ -59,15 +90,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng noviSad = new LatLng(45.2711884,19.7787146);
-        mMap.addMarker(new MarkerOptions().position(noviSad).title("Marker in Novi Sad"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(noviSad,10));
+        //LatLng noviSad = new LatLng(45.2711884,19.7787146);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(noviSad,15));
 
-        Polyline line = mMap.addPolyline(new PolylineOptions()
-                .add(new LatLng(45.2553797,19.8407819), new LatLng(45.2561198,19.8447123))
+
+        markerLatLng = new LatLng(45.2711884,19.7787146);
+
+        View marker = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_marker_layout, null);
+        TextView numTxt = (TextView) marker.findViewById(R.id.num_txt);
+        int count = 0;
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(placePositions.get(0),16));
+
+        for(PlaceInfo place : mRouteInfo.getPlaces()){
+
+            count++;
+            numTxt.setText(String.valueOf(count));
+
+            customMarker = mMap.addMarker(new MarkerOptions()
+                    .position(placePositions.get(count-1))
+                    .title(place.getPlaceTitle())
+                    .snippet("Snippet")
+                    .icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, marker))));
+
+        }
+
+        line  = mMap.addPolyline(new PolylineOptions()
+                .addAll(placePositions)
                 .width(5)
-                .color(Color.RED));
-
+                .color(ContextCompat.getColor(getApplicationContext(),R.color.green)));
     }
+
+    // Convert a view to bitmap
+    public static Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
 }
